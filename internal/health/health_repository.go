35da93bf2fb315666e83a988fc8378f2e6ba51c3
keerthana <keerthanaselvam05@gmail.com>
2024-12-16 14:46:30 +0530

    Integrate Logging Library

--- internal/health/health_repository.go
+++ internal/health/health_repository.go
@@ -6,21 +6,28 @@ import (
 
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/null-bd/department-service-api/internal/errors"
+	"github.com/null-bd/logger"
 )
 
 type HealthRepository struct {
-	db *pgxpool.Pool
+	db  *pgxpool.Pool
+	log logger.Logger
 }
 
-func NewHealthRepository(db *pgxpool.Pool) *HealthRepository {
-	return &HealthRepository{db: db}
+func NewHealthRepository(db *pgxpool.Pool, logger logger.Logger) *HealthRepository {
+	return &HealthRepository{
+		db:  db,
+		log: logger,
+	}
 }
 
 func (r *HealthRepository) CheckDatabase() error {
+	r.log.Debug("repository : CheckDatabase : begin", nil)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := r.db.Ping(ctx); err != nil {
 		return errors.NewDatabaseConnectionError(err)
 	}
+	r.log.Debug("repository : CheckDatabase : exit", nil)
 	return nil
 }
